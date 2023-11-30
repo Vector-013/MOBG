b397be6e2602b11a802c90e8c241e4dd93eb71a3
Vector-013 <vectorprime1397@gmail.com>
2023-11-30 22:32:19 +0530

    able to locate mouse click rows and columns on the board

--- game.py
+++ game.py
@@ -14,8 +14,11 @@ for i in range (8):
     ROW.append(17.5 + 123.75*i)
     COL.append(17.5 + 123.75*i)
 
+
+
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Chess Game")
+
 
 def redraw_gameWindow():
     global win
@@ -25,7 +28,19 @@ def redraw_gameWindow():
     play_board.draw(win)
     
     pygame.display.update()
+
+def click(pos):
+    """
+    return pos (x,y) of click in range 0-7 0-7
+    """
+    x = pos[0]
+    y = pos[1]
     
+    for i in range(8):
+        for j in range(8):
+            if x>=COL[i]-17.5 and x<=COL[i]+106.25 and y>=ROW[j]-17.5 and y<=ROW[j]+106.25:
+                print(i ,j)
+                return i, j   
     
 def main():
     
@@ -45,7 +60,8 @@ def main():
                 pass
             
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                r,c = click(pos)
             
 
 
